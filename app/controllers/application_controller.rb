683be3c3f25650be2bc1663e90ac564c6fc7659c
furuya-tomoki <furuya1122@gmail.com>
2021-04-02 16:13:37 +0000

    [Add]teble

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+   before_action :configure_permitted_parameters, if: :devise_controller?
+   # devise利用の機能
+    protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :image, :encrypted_password])
+    # ユーザー登録時に必要な値
+
+  end
 end
